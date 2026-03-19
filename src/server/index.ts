51f620157661575708bf6e31a3866cf28b695c87
user <jsjames7715@gmail.com>
2026-03-19 13:18:52 -0700

    Add logo, favicon, root redirect, cleanup

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { rpcApp } from "./routes/rpc";
 import { clientEntry } from "./routes/client-entry";
 
-const app = new Hono();
+app.get("/", (c) => c.redirect("/forum"));
 
 app.route("/rpc", rpcApp);
 app.get("/*", clientEntry);
